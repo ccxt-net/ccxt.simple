b171d0e452603e1c656702c151481ff8abd498d5
SEONGAHN_LEE <lisa@odinsoft.co.kr>
2022-12-02 18:32:03 +0900

    update function for network compare...

---
+++ src/Exchanges/GateIO/CoinState.cs
@@ -0,0 +1,13 @@
+﻿namespace CCXT.Simple.Exchanges.GateIO
+{   
+    public class CoinState
+    {
+        public string currency { get; set; }
+        public bool delisted { get; set; }
+        public bool withdraw_disabled { get; set; }
+        public bool withdraw_delayed { get; set; }
+        public bool deposit_disabled { get; set; }
+        public bool trade_disabled { get; set; }
+        public string chain { get; set; }
+    }
+}
